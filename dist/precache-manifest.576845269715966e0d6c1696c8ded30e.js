bba98b0697f8140c66f24257d2548fd746d7918e
dnyaneshwar <jawanednyaneshwar1@gmail.com>
2021-11-15 16:24:52 +0530

    huruko deploy

---
+++ dist/precache-manifest.576845269715966e0d6c1696c8ded30e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17d4c1ba24263201df6",
+    "url": "/css/app.6a19e8a7.css"
+  },
+  {
+    "revision": "089bb7146af17f1fca4b",
+    "url": "/css/chunk-958fdca8.0b9cf5b5.css"
+  },
+  {
+    "revision": "57d20ebf064c5c72802b7c24315796af",
+    "url": "/img/bg.57d20ebf.svg"
+  },
+  {
+    "revision": "a1468fce7ed59de805524cb0063682c4",
+    "url": "/img/mypic.a1468fce.jpg"
+  },
+  {
+    "revision": "bd2c175e58e548d1453fb5cbe5fff77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e17d4c1ba24263201df6",
+    "url": "/js/app.11821c1a.js"
+  },
+  {
+    "revision": "089bb7146af17f1fca4b",
+    "url": "/js/chunk-958fdca8.0c6cb3bc.js"
+  },
+  {
+    "revision": "1f6cf1b3ed0a65c1121c",
+    "url": "/js/chunk-vendors.833ad66b.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  }
+]);
